cb59c0e2d53295584eac2bf24dc36572c0c9c1e2
Garcia G. ELLA <gercienguimbi@gmail.com>
2026-03-15 02:57:25 +0100

    fix supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://xojdlpxyikajwnqtpoy.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvamRscHh5aWthanducXRweG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MzAyNzUsImV4cCI6MjA4OTEwNjI3NX0.-n3Rsy20QS_YKcaXhWL6zmSNbwrfsPUF2wgyOPjTCc'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
